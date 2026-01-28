10e5d22ae83fa1b819583754bf2c6b9e340ab880
DenisFeoktistov <dfeoktistov523@icloud.com>
2026-01-28 20:22:00 +0300

    feat: add chinese symbols image analyzer

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Use the official Python image as the base image
+FROM python:3.10-slim
+
+# Set environment variables for FastAPI
+ENV APP_HOME /app
+ENV PORT 5000
+
+# Create and set the working directory
+WORKDIR $APP_HOME
+
+COPY . $APP_HOME/
+# Copy the requirements file into the container
+COPY requirements.txt $APP_HOME/
+
+# Install any needed packages specified in requirements.txt
+RUN pip install -r requirements.txt
+
+# Expose the port on which the FastAPI application will run
+EXPOSE $PORT
+
+# Start the FastAPI application when the container is run
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "5000"]
---
+++ main.py
@@ -0,0 +1,100 @@
+import os
+import random
+import time
+
+import cv2
+
+from fastapi import FastAPI, HTTPException
+
+
+import uvicorn
+import re
+
+
+import aiohttp
+import pytesseract
+
+app = FastAPI()
+
+ERROR = "error"
+
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, binary_image = cv2.threshold(gray, 235, 255, cv2.THRESH_BINARY)
+
+    cv2.imwrite(image_path, binary_image)
+
+
+def generate_random_name():
+    return "images/" + ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(15))
+
+
+def filter_chinese_text(text):
+    chinese_pattern = re.compile(r'[\u4e00-\u9fa5]+')
+    chinese_matches = chinese_pattern.findall(text)
+    return ''.join(filter(lambda x: x != "一" and x != "二" and x != "三" and x != "人", chinese_matches))
+
+
+async def download_image(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                image_data = await response.read()
+
+                name = generate_random_name() + ".jpg"
+
+                with open(name, 'wb') as image_file:
+                    image_file.write(image_data)
+
+                return name
+            else:
+                return ERROR
+
+
+@app.get('/chinese_symbols_image_analyse/process_image')
+async def process_image(image_url: str):
+    image_path = await download_image(image_url)
+
+    if image_path == ERROR:
+        raise HTTPException(status_code=400, detail="Image download failed")
+
+    preprocess_image(image_path)
+
+    custom_config = "--oem 3 --psm 6"
+    data = pytesseract.image_to_data(image_path, lang="chi_tra", config=custom_config, output_type=pytesseract.Output.DICT)
+
+    # print(data)
+
+    # os.remove(image_path)
+
+    cnt = 0
+    for i in range(len(data['text'])):
+        # print(row)
+        if not filter_chinese_text(data["text"][i]):
+            continue
+
+        confidence = data['conf'][i]
+        print(confidence, data["text"][i])
+
+        if confidence > 85:
+            cnt += 1
+
+            if cnt >= 3:
+                return False
+
+    return True
+
+
+if __name__ == "__main__":
+    import shutil
+
+    directory_path = "images"  # Replace with your directory path
+    shutil.rmtree(directory_path)
+
+    os.mkdir("images")
+    uvicorn.run(app, host="0.0.0.0", port=5000)
